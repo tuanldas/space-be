bd6ba01291d646aa28364df3320e9bab81ccc46c
Tuanldas <tuanldas@gmail.com>
2024-11-04 16:29:15 +0700

    Thêm vite service khi run dev

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
