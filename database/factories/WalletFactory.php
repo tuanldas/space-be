8201feaea4645337f90b7ad61f71ed6bee7ddb1a
tuanldas <tuanldas@gmail.com>
2025-01-13 13:45:41 +0700

    Bổ sung images table để quản lý

--- database/factories/WalletFactory.php
+++ database/factories/WalletFactory.php
@@ -3,8 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Storage;
-use PHPUnit\TextUI\XmlConfiguration\MigrationException;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Wallet>
@@ -18,23 +16,12 @@ class WalletFactory extends Factory
      */
     public function definition(): array
     {
-        $files = Storage::disk('local')->files('server/wallets/icons');
-
-        $images = array_filter($files, function ($file) {
-            return in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), ['jpg', 'jpeg', 'png', 'gif']);
-        });
-        if (!empty($images)) {
-            $randomImage = $images[array_rand($images)];
-            return [
-                'name' => $this->faker->name(),
-                'icon' => $randomImage,
-                'balance' => $this->faker->randomFloat(2, 0, 1000000),
-                'currency' => $this->faker->randomElement(['VND', 'USD', 'EUR']),
-                'type' => $this->faker->randomElement(['personal']),
-                'created_by' => \App\Models\User::factory(),
-            ];
-        } else {
-            throw new MigrationException('Không tìm thấy ảnh nào trong thư mục wallets/icons.');
-        }
+        return [
+            'name' => $this->faker->name(),
+            'balance' => $this->faker->randomFloat(2, 0, 1000000),
+            'currency' => $this->faker->randomElement(['VND', 'USD', 'EUR']),
+            'type' => $this->faker->randomElement(['personal']),
+            'created_by' => \App\Models\User::factory(),
+        ];
     }
 }
