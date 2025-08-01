2ad6eb7d61d9ea052cc8426f3ba727142b82e798
tuanldas <tuanldas@gmail.com>
2025-08-01 16:00:41 +0700

    Thêm Enum AbilityType và các file ngôn ngữ cho quyền

---
+++ lang/vi/abilities.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Ability Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for ability titles that we need to
+    | display to the user. You are free to modify these language lines according
+    | to your application's requirements.
+    |
+    */
+
+    // User Management
+    'view-users' => 'Xem danh sách người dùng',
+    'create-users' => 'Tạo người dùng mới',
+    'update-users' => 'Cập nhật thông tin người dùng',
+    'delete-users' => 'Xóa người dùng',
+
+    // Role Management
+    'manage-roles' => 'Quản lý vai trò',
+
+    // Settings
+    'manage-settings' => 'Quản lý cài đặt hệ thống',
+
+    // Transaction Categories
+    'view-transaction-categories' => 'Xem danh sách danh mục giao dịch',
+    'create-transaction-categories' => 'Tạo danh mục giao dịch mới',
+    'update-transaction-categories' => 'Cập nhật danh mục giao dịch',
+    'delete-transaction-categories' => 'Xóa danh mục giao dịch',
+    'restore-transaction-categories' => 'Khôi phục danh mục giao dịch đã xóa',
+    'force-delete-transaction-categories' => 'Xóa vĩnh viễn danh mục giao dịch',
+    'manage-default-transaction-categories' => 'Quản lý danh mục giao dịch mặc định',
+]; 
