b7b03aa15d03aeaf9287dd0cfd011833129ed72e
tuanldas <tuanldas@gmail.com>
2025-08-04 11:32:29 +0700

    Xóa TransactionCategorySeeder vì không còn cần thiết do đã có migration tạo danh mục mặc định với hình ảnh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            TransactionCategorySeeder::class,
             WalletSeeder::class,
         ]);
     }
